a09b258e118e5917dc821749151aeb58dbbfa14b
Suji <88776465+suji-datahub@users.noreply.github.com>
2026-09-13 09:49:19 +0100

    Update data_import_troubleshooting.sql

--- data_import_troubleshooting.sql.sql
+++ data_import_troubleshooting.sql.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE '/Users/suji/Downloads/고객(SQL) - customers_export.csv.csv' 
+LOAD DATA LOCAL INFILE '/Users/suji/Desktop/데이터분석/포트폴리오/KODDYLONDON/customer.csv' 
 INTO TABLE customer
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
@@ -6,7 +6,7 @@ ESCAPED BY '\\'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE '/Users/suji/Downloads/주문(SQL) - orders_export_1.csv.csv'
+LOAD DATA LOCAL INFILE '/Users/suji/Desktop/데이터분석/포트폴리오/KODDYLONDON/orders.csv'
 INTO TABLE orders -- 실제 테이블명으로 변경
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
@@ -14,7 +14,7 @@ ESCAPED BY '\\'
 LINES TERMINATED BY '\r\n' -- 쇼피파이/엑셀 CSV 필수 줄바꿈 옵션
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE '/Users/suji/Downloads/상품(SQL) - 상품 (1).csv'
+LOAD DATA LOCAL INFILE '/Users/suji/Desktop/데이터분석/포트폴리오/KODDYLONDON/product.csv'
 INTO TABLE product
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
