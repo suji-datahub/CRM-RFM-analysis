36e822a80856d4cab146fdeae8415322b824f9bd
Suji <88776465+suji-datahub@users.noreply.github.com>
2026-09-13 10:17:37 +0100

    Update data_import_troubleshooting.sql

--- data_import_troubleshooting.sql
+++ data_import_troubleshooting.sql
@@ -7,11 +7,11 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE '/Users/suji/Desktop/데이터분석/포트폴리오/KODDYLONDON/orders.csv'
-INTO TABLE orders -- 실제 테이블명으로 변경
+INTO TABLE orders
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
 ESCAPED BY '\\'
-LINES TERMINATED BY '\r\n' -- 쇼피파이/엑셀 CSV 필수 줄바꿈 옵션
+LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE '/Users/suji/Desktop/데이터분석/포트폴리오/KODDYLONDON/product.csv'
@@ -19,5 +19,5 @@ INTO TABLE product
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
 ESCAPED BY '\\'
-LINES TERMINATED BY '\n'  -- \r\n 에서 \n 으로 변경
+LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
